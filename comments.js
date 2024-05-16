6fef648eaf199fe9046043092ae8df22039a955c
Tomasz Łucewicz <151016398+tomasz-lucewicz-swo@users.noreply.github.com>
2024-05-16 07:38:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comments', (req, res) => {
+    res.send('This is a comments page');
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000/comments');
+});
+
+//
